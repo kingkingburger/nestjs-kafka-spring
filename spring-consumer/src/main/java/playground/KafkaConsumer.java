77e8e962f2e0ff4df4c3cca27bce805e039dc1ea
minho <dnjsalsgh123@gmail.com>
2025-08-12 14:02:14 +0900

    fix: duplicated alarm topic

--- spring-consumer/src/main/java/playground/KafkaConsumer.java
+++ spring-consumer/src/main/java/playground/KafkaConsumer.java
@@ -1,31 +1,15 @@
 package playground;
 
 import java.io.IOException;
-import java.time.LocalDateTime;
-import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-import playground.alarm.AlarmDto;
-import playground.alarm.AlarmService;
 import playground.dto.KafkaMessageDto;
 
 @Component
-@RequiredArgsConstructor
 public class KafkaConsumer {
 
-  private final AlarmService alarmService;
-
-  @KafkaListener(topics = "alarm", groupId = "spring-consumer-group")
+  @KafkaListener(topics = "my-first-topic", groupId = "spring-consumer-group")
   public void consume(KafkaMessageDto kafkaMessage) throws IOException {
     System.out.println("수신된 원본 메시지: " + kafkaMessage);
-
-    // KafkaMessageDto -> AlarmDto 로 변환
-    AlarmDto alarmDto = new AlarmDto();
-    alarmDto.setAlarmId(String.valueOf(kafkaMessage.getId())); // long -> String 변환
-    alarmDto.setMessage(kafkaMessage.getText());
-    alarmDto.setCreatedAt(LocalDateTime.now()); // 수신한 시각을 생성 시각으로 설정
-
-    // 변환된 DTO를 사용하여 비즈니스 로직 처리
-    alarmService.processAlarm(alarmDto);
   }
 }
--- spring-consumer/src/main/java/playground/alarm/AlarmConsumer.java
+++ spring-consumer/src/main/java/playground/alarm/AlarmConsumer.java
@@ -1,14 +1,31 @@
 package playground.alarm;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import playground.dto.KafkaMessageDto;
 
 @Component
+@RequiredArgsConstructor
 public class AlarmConsumer {
 
-  @KafkaListener(topics = "alarm", groupId = "alarm-group")
-  public void consume(String message) throws IOException {
-    System.out.println(String.format("알람: %s", message));
+  private final AlarmService alarmService;
+
+  @KafkaListener(topics = "alarm", groupId = "spring-consumer-group")
+  public void consume(KafkaMessageDto kafkaMessage) throws IOException {
+    System.out.println("수신된 원본 메시지: " + kafkaMessage);
+
+    // KafkaMessageDto -> AlarmDto 로 변환
+    AlarmDto alarmDto = new AlarmDto();
+    alarmDto.setAlarmId(String.valueOf(kafkaMessage.getId())); // long -> String 변환
+    alarmDto.setMessage(kafkaMessage.getText());
+    alarmDto.setCreatedAt(LocalDateTime.now()); // 수신한 시각을 생성 시각으로 설정
+
+    System.out.println("변환된 AlarmDto: " + alarmDto);
+
+    // 변환된 DTO를 사용하여 비즈니스 로직 처리
+    alarmService.processAlarm(alarmDto);
   }
 }
