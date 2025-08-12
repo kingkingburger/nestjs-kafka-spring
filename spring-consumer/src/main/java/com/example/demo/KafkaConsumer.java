028af65f2374a13bc10f6d2f78c9d358e20f1ff4
minho <dnjsalsgh123@gmail.com>
2025-08-12 09:41:48 +0900

    formatting

--- spring-consumer/src/main/java/com/example/demo/KafkaConsumer.java
+++ spring-consumer/src/main/java/com/example/demo/KafkaConsumer.java
@@ -1,15 +1,14 @@
 package com.example.demo;
 
+import java.io.IOException;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Component
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "my-first-topic", groupId = "spring-consumer-group")
-    public void consume(String message) throws IOException {
-        System.out.println(String.format("Consumed message: %s", message));
-    }
+  @KafkaListener(topics = "my-first-topic", groupId = "spring-consumer-group")
+  public void consume(String message) throws IOException {
+    System.out.println(String.format("Consumed message: %s", message));
+  }
 }
