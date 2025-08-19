35137de367fa6a0e913ebb27d2e6612d92829d4f
minho <dnjsalsgh123@gmail.com>
2025-08-19 10:33:49 +0900

    update: setting the k6 recycling process in a container

--- nest-producer/src/k6Test/script.js
+++ nest-producer/src/k6Test/script.js
@@ -19,12 +19,10 @@ export default function () {
   // GET 요청을 보냅니다.
   const res = http.get(url);
 
-  console.log('res = ' , res);
   // (선택 사항) 응답이 성공적(status code 200)인지 확인합니다.
   check(res, {
     'status is 200': (r) => r.status === 200,
   });
-
   // 각 요청 사이에 1초간 대기합니다. (서버에 과도한 부하를 주지 않기 위함)
   // 더 빠르게 보내고 싶다면 이 값을 줄이거나 sleep 함수를 제거할 수 있습니다.
   // sleep(0.1);
